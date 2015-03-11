6c1dfc9af4961e0360a7eca8c66d7c845b3d2229
Aayush Singhal <aayushsinghal90@gmail.com>
2015-03-11 23:53:05 +0530

    Adding class structure.

---
+++ dummy/aggregator.py
@@ -0,0 +1,17 @@
+class Aggregator():
+	"""
+	Database of the mixture of 2d and 3d models with settings of each model.
+	"""
+	models = []
+	
+	def merger(self, model, model):
+		pass
+	def update():
+	def save():
+	def close():
+	def open():
+	
+	class Feature():
+		def area():
+		def intersect():
+		pass
