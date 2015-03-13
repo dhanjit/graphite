2d9ef368c795b90ccb1c8bdf3f0df138c12b039a
Vineet <vvineett@gmail.com>
2015-03-13 18:05:15 +0530

    model added

--- src/model.py
+++ src/model.py
@@ -1,25 +1,39 @@
+from sympy import *
+
 class Model():
 	"""
 	Represents the model of the equation.
 	@sympyexpression - parse expression
 	@integer - number of variables
 	"""
+	def __init__(self):
+		self.initAttr()
+
+	def initAttr(self):
+		self.settings = {}
+		self.expression = ""
+		self.numVar = -1
+		self.errors = []
+
+class Model2D(Model):
 	
-	self.settings = null
-	self.canvas = null
-	self.expression = null
-	self.errors = []
+	def __init__(self):
+		super(Model2D, self).__init__()
+		self.numVar = 2
+
+	def eval(self,xVal):
+		x = Symbol('x')
+		f = lambdify(x,self.expression,"numpy")
+		self.dataPoints = f(x)
+		pass
+
+class Model3D(Model):
+
+	def __init__(self):
+		super(Model2D, self).__init__()
+		self.numVar = 3
 
 	def eval(self):
 		pass
-	 
-
-
-class Model2D(Model):
-	pass
-
-
-class Model3D(Model):
-	pass
 	
 
