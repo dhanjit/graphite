06ff177caecba42f9fb5b796f28afeeaa9f18b59
Aayush Singhal <aayushsinghal90@gmail.com>
2015-03-15 01:53:03 +0530

    input Interface

---
+++ inputInterface/exp.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'exp.ui'
+#
+# Created: Sat Mar 14 00:03:22 2015
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(QtGui.QWidget):
+    def setupUi(self):
+        self.setObjectName(_fromUtf8("Form"))
+        self.resize(362, 148)
+        self.widget = QtGui.QWidget(self)
+        self.widget.setGeometry(QtCore.QRect(20, 30, 311, 95))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.gridLayout = QtGui.QGridLayout(self.widget)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.pushButton = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.pushButton_4 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.gridLayout.addWidget(self.pushButton_4, 0, 1, 1, 1)
+        self.pushButton_5 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_5.sizePolicy().hasHeightForWidth())
+        self.pushButton_5.setSizePolicy(sizePolicy)
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
+        self.gridLayout.addWidget(self.pushButton_5, 0, 2, 1, 1)
+        self.pushButton_6 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_6.sizePolicy().hasHeightForWidth())
+        self.pushButton_6.setSizePolicy(sizePolicy)
+        self.pushButton_6.setObjectName(_fromUtf8("pushButton_6"))
+        self.gridLayout.addWidget(self.pushButton_6, 1, 0, 1, 1)
+        self.pushButton_7 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_7.sizePolicy().hasHeightForWidth())
+        self.pushButton_7.setSizePolicy(sizePolicy)
+        self.pushButton_7.setObjectName(_fromUtf8("pushButton_7"))
+        self.gridLayout.addWidget(self.pushButton_7, 1, 1, 1, 1)
+        self.pushButton_8 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_8.sizePolicy().hasHeightForWidth())
+        self.pushButton_8.setSizePolicy(sizePolicy)
+        self.pushButton_8.setObjectName(_fromUtf8("pushButton_8"))
+        self.gridLayout.addWidget(self.pushButton_8, 1, 2, 1, 1)
+        self.pushButton_9 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_9.sizePolicy().hasHeightForWidth())
+        self.pushButton_9.setSizePolicy(sizePolicy)
+        self.pushButton_9.setObjectName(_fromUtf8("pushButton_9"))
+        self.gridLayout.addWidget(self.pushButton_9, 2, 0, 1, 1)
+        self.pushButton_10 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_10.sizePolicy().hasHeightForWidth())
+        self.pushButton_10.setSizePolicy(sizePolicy)
+        self.pushButton_10.setObjectName(_fromUtf8("pushButton_10"))
+        self.gridLayout.addWidget(self.pushButton_10, 2, 1, 1, 1)
+        self.pushButton_11 = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_11.sizePolicy().hasHeightForWidth())
+        self.pushButton_11.setSizePolicy(sizePolicy)
+        self.pushButton_11.setObjectName(_fromUtf8("pushButton_11"))
+        self.gridLayout.addWidget(self.pushButton_11, 2, 2, 1, 1)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self):
+        self.setWindowTitle(_translate("Form", "Form", None))
+        self.pushButton.setText(_translate("Form", "e^x", None))
+        self.pushButton_4.setText(_translate("Form", "x^y", None))
+        self.pushButton_5.setText(_translate("Form", "x^2", None))
+        self.pushButton_6.setText(_translate("Form", "x^3", None))
+        self.pushButton_7.setText(_translate("Form", "√a", None))
+        self.pushButton_8.setText(_translate("Form", "cube root(x)", None))
+        self.pushButton_9.setText(_translate("Form", "n root(x)", None))
+        self.pushButton_10.setText(_translate("Form", "ENTER", None))
+        self.pushButton_11.setText(_translate("Form", "CLEAR", None))
+
+    def getInput(self):
+        return ""
+
+    def __call__(self):
+        self.setupUi()
+        self.show()
+
