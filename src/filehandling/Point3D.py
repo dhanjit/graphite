a25a6a0d0edabbdc47d79130b06ee1a138201cb1
Buddha Prakash <bprakashjsr@gmail.com>
2015-03-19 23:34:55 +0530

    Added fileHandler handle module. Module returns a list of Points

---
+++ src/filehandling/Point3D.py
@@ -0,0 +1,17 @@
+__author__ = 'batman'
+
+class Point3D:
+  
+  def __init__(self,valx,valy,valz):
+      self.x=valx;
+      self.y=valy;
+      self.z=valz;
+
+  def getX(self):
+      return self.x;
+
+  def getY(self):
+      return self.y;
+
+  def getZ(self):
+      return self.z;
