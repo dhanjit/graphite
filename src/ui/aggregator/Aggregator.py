ed208e5d0bffdb5e596900e171ed092bff69d2e3
Dhanjit <dhanjitdas1@gmail.com>
2015-03-20 14:40:04 +0530

    aggregator widget. To be completely done. Control left over to the controller

--- src/ui/aggregator/Aggregator.py
+++ src/ui/aggregator/Aggregator.py
@@ -1,1 +1,24 @@
-__author__ = 'batman'
+from PyQt4 import QtGui
+from src.Settings import *
+
+class Aggregator(QtGui.QWidget):
+#	def __init__(self, controller):
+	def __init__(self):
+		super(Aggregator, self).__init__()
+		self.models = []
+		self.domain = []
+#		self.controller = controller  # Check gargbage collection. Causes problems.
+		self.settings = Settings()
+		self.initUI()
+
+	def initUI(self):
+		pass
+
+	def addModel(self, model):
+		self.models.append(model)
+
+	def genSelectedModelData(self):
+		for model in self.models:
+			if model.visible:
+				data = model.eval(self.domain)
+				yield data
