9686ef0aad4ca01f68ec7dea6db9fb74154eb8e8
Dhanjit <dhanjitdas1@gmail.com>
2015-03-22 01:14:07 +0530

    3d scatter library error

--- src/ui/canvas/Canvas3D.py
+++ src/ui/canvas/Canvas3D.py
@@ -2,7 +2,7 @@ from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from Canvas import Canvas
 from mpl_toolkits.mplot3d import Axes3D
-#import numpy as np
+import numpy as np
 from matplotlib import cm
 class Canvas3D(Canvas):
 	def __init__(self, parent=None, width=6.5, height=5.5, dpi=100, sharex=None, sharey=None, fig=None):
@@ -25,8 +25,15 @@ class Canvas3D(Canvas):
 		print len(plottable3D.x)
 		print len(plottable3D.y)
 		print len(plottable3D.z)
+
+		x = np.random.random_sample(100)
+		y = np.random.random_sample(100)
+		z = np.random.random_sample(100)
+
+		self.axes.
 		#self.axes.scatter(self, plottable3D.x, plottable3D.y, plottable3D.z, c='r', marker='o')
-		self.axes.plot_surface(plottable3D.x, plottable3D.y, plottable3D.z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+		self.axes.scatter(self, x, y, z, c='r', marker='o')
+		#self.axes.plot_surface(plottable3D.x, plottable3D.y, plottable3D.z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 			#self.draw()
 
 	def updateSettings(self):
