88773c9199b55aba921d563419f0d3c6264790b7
Dhanjit <dhanjitdas1@gmail.com>
2015-03-21 17:17:13 +0530

    current type and getPlottables

--- src/ui/aggregator/Aggregator.py
+++ src/ui/aggregator/Aggregator.py
@@ -7,6 +7,7 @@ class Aggregator(QtGui.QWidget):
 
 	def __init__(self):
 		super(Aggregator, self).__init__()
+		self.currenttype = None
 #		self.controller = controller  # Check gargbage collection. Causes problems.
 		self.settings = Settings()
 		self.initDomain()
@@ -24,15 +25,30 @@ class Aggregator(QtGui.QWidget):
 		self.domain['y'] = arange(-5, 5, 0.25)
 		self.domain['z'] = arange(0.0, 3.0, 0.1)
 
-	def genSelectedModelData(self):
-		for model in self.models:
-			if model.visible:
-				data = model.eval(self.domain)
-				yield data
-
 	def clearSelection(self):
 		for model in self.models:
 			model.visible = False
 
 	def selectModel(self, index):
 		self.models[index].visible = True
+#		self.updateCurrentType()
+
+	def updateCurrentType(self):
+		is2D = [ model.type == '2D' for model in self.models if model.visible ]
+		if all(is2D):
+			self.currenttype = '2D'
+		elif not any(is2D):
+			self.currenttype = '3D'
+		else:
+			self.currenttype = False
+
+	def getCurrentType(self, refresh=True, default=False):
+		if refresh:
+			self.updateCurrentType()
+		return default if not self.currenttype else self.currenttype
+
+
+	def getPlottables(self):
+		return (model.getPlottable(type=self.currenttype, domain=self.domain) for model in self.models if model.visible)
+
+
