d21a4412a73a51a36938d8406cd30b1ffba8571f
Dhanjit <dhanjitdas1@gmail.com>
2015-03-21 17:15:59 +0530

    canvas superclass of 2d and 3d. Plot function now with each canvas

--- src/ui/canvas/Canvas3D.py
+++ src/ui/canvas/Canvas3D.py
@@ -1,11 +1,12 @@
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
-
-class Canvas3D(FigureCanvas):
+from Canvas import Canvas
+#import numpy as np
+from matplotlib import cm
+class Canvas3D(Canvas):
 	def __init__(self, parent=None, width=6.5, height=5.5, dpi=100, sharex=None, sharey=None, fig=None):
 		self.fig = Figure(figsize=(width, height), dpi=dpi, facecolor='#FFFFFF')
-		FigureCanvas.__init__(self, self.fig)
+		super(Canvas3D,self).__init__(figure=self.fig)
 		#super(Canvas3D, self).__init__(self.fig)
 
 		self.axes = self.fig.add_subplot(111, projection='3d')
@@ -17,3 +18,10 @@ class Canvas3D(FigureCanvas):
 		#	QtGui.QSizePolicy.Expanding,
 		#	QtGui.QSizePolicy.Expanding)
 		FigureCanvas.updateGeometry(self)
+
+	def plot(self, plottable3D):
+		self.axes.mouse_init()
+		for data in plottable3D:
+			#self.axes.scatter(self.data['x'], self.data['y'], self.data['z'], c='r', marker='o')
+			self.axes.plot_surface(data['x'], data['y'], data['z'], rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+			#self.draw()
