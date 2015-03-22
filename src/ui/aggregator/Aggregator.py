d181065c22f98546e5e9584101945c50da9669e1
Aayush Singhal <aayushsinghal90@gmail.com>
2015-03-22 22:06:37 +0530

    Model Settings added

--- src/ui/aggregator/Aggregator.py
+++ src/ui/aggregator/Aggregator.py
@@ -1,22 +1,54 @@
 from PyQt4 import QtGui
 from src.Settings import *
 from numpy import arange
+from settingsTable import *
 
 class Aggregator(QtGui.QWidget):
 	models = []
+	model_settings = []
+	settings_Table = []
+	functions = []
+	settings_btn = []
+
 
 	def __init__(self):
 		super(Aggregator, self).__init__()
-#		self.controller = controller  # Check gargbage collection. Causes problems.
+		#self.controller = controller  #Check gargbage collection. Causes problems.
+		self.mainLayout = QtGui.QVBoxLayout()
+		# self.mainLayout = QtGui.QListView()
 		self.settings = Settings()
 		self.initDomain()
-		self.initUI()
+		#self.initUI()
 
-	def initUI(self):
-		pass
+
+	def initUI(self,model):
+		hbox = QtGui.QHBoxLayout()
+		widget = QtGui.QWidget()
+		function = QtGui.QCheckBox("Function")
+		function.setStyleSheet("color: black; background-color: red; font: bold")
+		function.setChecked(True)
+		self.functions.append(function)
+		btn = QtGui.QPushButton("Set")
+		btn.setMaximumWidth(30)
+		btn.setEnabled(True)
+		self.settings_btn.append(btn)
+		hbox.addWidget(function)
+		hbox.addWidget(btn)
+		groupbox = QtGui.QGroupBox()
+		self.mainLayout.addLayout(hbox)
+		self.mainLayout.addStretch(1)
+		self.setLayout(self.mainLayout)
+
+		self.connect(btn,QtCore.SIGNAL("clicked()"),self.showpop)
+		self.connect(function,QtCore.SIGNAL("stateChanged(int)"),self.select)
+
+
+
 
 	def addModel(self, model):
 		self.models.append(model)
+		self.initUI(model)
+		self.setDefaultSetting()
 
 	def initDomain(self, init={}):
 		self.domain = init
@@ -36,3 +68,33 @@ class Aggregator(QtGui.QWidget):
 
 	def selectModel(self, index):
 		self.models[index].visible = True
+
+	def showTable(self):
+		for i in range(len(self.settings_btn)):
+			if(self.sender()==self.settings_btn[i]):
+				if(self.settings_Table[i].isVisible()):
+					self.settings_Table[i].setVisible(False)
+				else:
+					self.settings_Table[i].setVisible(True)
+		self.initUI()
+
+	def showpop(self):
+		for i in range(len(self.settings_btn)):
+			if(self.sender()==self.settings_btn[i]):
+				self.table = ModelSettingsTable(self.model_settings[i])
+
+	def setDefaultSetting(self):
+		set = Settings()
+		set["Color"] = QtGui.QColor("red")
+		set["Width"] = 1
+		set["Line fill"] = "-"
+		set["Transparency"] = 1.0
+		self.model_settings.append(set)
+
+	def select(self):
+		for i in range(len(self.functions)):
+			if self.functions[i].isChecked():
+				self.settings_btn[i].setEnabled(True)
+			else:
+				self.settings_btn[i].setEnabled(False)
+
--- src/ui/viewport/Viewport.py
+++ src/ui/viewport/Viewport.py
@@ -1,6 +1,7 @@
 from PyQt4.QtGui import QWidget, QHBoxLayout
 from src.ui.canvas.Canvas3D import Canvas3D
 from src.ui.canvas.Canvas2D import Canvas2D
+# from aggregateSettings import *
 
 
 class Viewport(QWidget):
@@ -11,13 +12,14 @@ class Viewport(QWidget):
 		self.canvas3d = Canvas3D()
 
 		self.canvas = self.canvas2d #default show 2d
-		#	    self.settingsWidget
+		# self.settingsWidget = Customize()
 		self.initUI()
 
 	def initUI(self):
 		hbox_layout = QHBoxLayout()
 		hbox_layout.addWidget(self.canvas)
-		hbox_layout.addStretch()
+		# hbox_layout.addWidget(self.settingsWidget)
+		#hbox_layout.addStretch()
 		self.setLayout(hbox_layout)
 
 	def updateView(self, aggregator):
