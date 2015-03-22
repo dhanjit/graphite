d0e52cc1ede832e3c8b6000a89d03e10188a3602
Dhanjit <dhanjitdas1@gmail.com>
2015-03-23 03:00:30 +0530

    stretched function file tabs

--- src/ui/input/Input.py
+++ src/ui/input/Input.py
@@ -1,18 +1,34 @@
-from PyQt4.QtGui import QTabWidget
+from PyQt4.QtGui import QTabWidget, QTabBar
+from PyQt4.QtCore import QSize
 from src.ui.input.Calculator import Calculator
 from src.ui.input.FileVisualizer import FileVisualizer
+
+
 class Input(QTabWidget):
 	def __init__(self, controller):
-		super(Input,self).__init__()
+		super(Input, self).__init__()
 		self.filevisualizer = FileVisualizer(controller)
 		self.calculator = Calculator(controller)
 		self.initUI()
 
 	def initUI(self):
-		self.addTab(self.calculator,"Function")
-		self.addTab(self.filevisualizer,"File")
+		tabbar = self.TabBarStretched()
+		self.addTab(self.calculator, "Function")
+		self.addTab(self.filevisualizer, "File")
+		tabbar.addTab("Function")
+		tabbar.addTab("File")
+		self.setTabBar(tabbar)
 
-#		size = QSize(0,0)
+	class TabBarStretched(QTabBar):
+		def tabSizeHint(self, index):
+			size = QSize(0, 0)
+			qr = self.frameGeometry()
+
+			for i in range(self.count() - 1):
+				size += QTabBar.tabSizeHint(self, i)
+			return QSize(qr.width() / self.count(), 30)
+
+# size = QSize(0,0)
 #		qt = self.tabBar().frameGeometry()
 #		self.tabBar().tabSizeHint()
 
