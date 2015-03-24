a3606060a4da6aa1a56524e8eeb88f6a0e022b78
Dhanjit <dhanjitdas1@gmail.com>
2015-03-24 14:59:15 +0530

    controller package to move controller,settings to another package instead of package root

---
+++ graphite/controller/Controller.py
@@ -0,0 +1,55 @@
+from graphite.ui.viewport.Viewport import Viewport
+from graphite.ui.aggregator.Aggregator import Aggregator
+from graphite.ui.input.Input import Input
+from graphite.ui.Tab import Tab
+from graphite.model.ModelCreator import ModelCreator
+from graphite.controller.globalSettings import Customize
+
+
+class Controller():
+	
+	def __init__(self, inputhandler):
+		self.aggregator = Aggregator(self)
+		self.input = Input(self, completer=inputhandler.completer)
+		self.viewport = Viewport()
+
+		self.global_settings = {} #just for passing to Customize, will be removed 
+		self.settings = Customize(self)
+
+		self.tabid = -1
+		self.tabs = None
+
+		self.tab = Tab(self.aggregator, self.input, self.viewport,self.settings)
+		self.inputhandler = inputhandler
+
+	def plotInput(self, input, isfile):
+		model = self.createModel(input, isfile)
+		if isfile:
+			self.input.filevisualizer.setTableData(plottable=model.getPlottable())
+		self.addModelToAggregator(model)
+		self.aggregator.updateCurrentType()
+		self.updateViewport()
+
+	def createModel(self, input, isfile):
+		if isfile:
+			model = ModelCreator.createModelFromFile(self.inputhandler.fileformathandler, input)
+		else:
+			model = ModelCreator.createModelFromExpression(self.inputhandler.parser, input)
+		return model
+
+	def addModelToAggregator(self, model):
+		self.aggregator.addModel(model)
+		self.tabs.setTabText(self.tabid, str(model.expression) or model.filename)
+		# self.aggregator.clearSelection()
+		# self.aggregator.selectModel(-1) #-1 for last model
+		print('model added to aggregator')
+
+	def updateViewport(self):
+		self.viewport.updateSettings(self.global_settings)
+		print("canvas type:", self.viewport.canvastype)
+		self.viewport.updateCanvas(self.aggregator)
+		self.viewport.showCanvas()
+#		self.viewport.canvas.axes.clear()
+#		self.draw_canvas()
+
+
