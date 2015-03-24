21ac7062231e9373bf6ea7e34c0b88aefef2a289
Aayush Singhal <aayushsinghal90@gmail.com>
2015-03-25 00:34:43 +0530

    nothing

--- graphite/ui/canvas/Canvas2D.py
+++ graphite/ui/canvas/Canvas2D.py
@@ -13,8 +13,13 @@ class Canvas2D(Canvas):
 		self.axes = self.fig.add_subplot(111)
 		self.setParent(parent)
 
-		# self.axes.xlabel('xlabel')
-		# self.axes.ylabel('ylabel')
+		# self.axes.xaxis.set_ticks_position('bottom')
+		# self.axes.spines['bottom'].set_position(('data',0))
+		# self.axes.yaxis.set_ticks_position('left')
+		# self.axes.spines['left'].set_position(('data',0))
+		#
+		# self.axes.set_xlabel('xlabel',fontsize = 20)
+		# self.axes.set_ylabel('ylabel',fontsize = 20)
 		
 		#FigureCanvas.setSizePolicy(self,
 			#QtGui.QSizePolicy.Expanding,
